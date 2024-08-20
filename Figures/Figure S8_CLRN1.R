8b2f73c10e12e698adf9833d396d648e8a250c58
erikdevrieze <158345516+erikdevrieze@users.noreply.github.com>
2024-08-20 15:22:34 +0200

    Update Figure S8_CLRN1.R

--- Figures/Figure S8_CLRN1.R
+++ Figures/Figure S8_CLRN1.R
@@ -10,8 +10,6 @@ library(rtracklayer)
 library(xlsx) 
 
 
-setwd("/Users/erikdevrieze/Library/CloudStorage/OneDrive-Radboudumc/z918116/UMCN/Manuscripts/2024 - USH isoseq manuscript/Analysis paper 2024")
-
 
 #####  Visualize from data GTF  #################
 
